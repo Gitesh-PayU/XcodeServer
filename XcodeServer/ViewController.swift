a3e2010ae3d8467a0a46095a6bcdc5bd229f36cd
Gitesh <gitesh.agarwal@payu.in>
2018-10-14 22:34:24 +0530

    add gitignore and initial code and pod setup

--- XcodeServer/ViewController.swift
+++ XcodeServer/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func buttonClicked(_ sender: Any) {
+    }
+    
 }
 
