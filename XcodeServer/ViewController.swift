997f2f589583a2dd1787c48499b13579a4eb8fea
Gitesh <gitesh.agarwal@payu.in>
2018-10-14 22:41:54 +0530

    added image fetch and remove button

--- XcodeServer/ViewController.swift
+++ XcodeServer/ViewController.swift
@@ -7,17 +7,30 @@
 //
 
 import UIKit
+import SDWebImage
 
 class ViewController: UIViewController {
+    
+    let urlString = "https://www.shareicon.net/data/2016/06/01/577179_checkmark_128x128.png"
 
     @IBOutlet weak var image: UIImageView!
     @IBOutlet weak var button: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.button.setTitle("Pull", for: .normal)
     }
 
-    @IBAction func buttonClicked(_ sender: Any) {
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        let title = button.title(for: .normal)
+        if title == "Remove"{
+            self.image.image = nil
+            self.button.setTitle("Pull", for: .normal)
+        }else{
+            let url = URL.init(string: urlString)
+            self.image.sd_setImage(with: url) { (image, error, type, url) in
+                self.button.setTitle("Remove", for: .normal)
+            }
+        }
     }
     
 }
